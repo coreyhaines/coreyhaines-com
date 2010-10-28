b3a5c745e209a87dec2385c0558376af90011979
coreyhaines <coreyhaines@gmail.com>
2010-10-28 15:56:10 +0200

    Update layout, start building sections

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -1,7 +1,7 @@
-$(function() {
-  $("#tripit-badge *").removeAttr("style");
-  $("#tripit-badge img").replaceWith("Tripit - ");
-});
+// $(function() {
+  // $("#tripit-badge *").removeAttr("style");
+  // $("#tripit-badge img").replaceWith("Tripit - ");
+// });
 $(function() {
   Galleria.loadTheme('/javascripts/galleria/theme/galleria.dots.js');
   $('.images').galleria();
@@ -14,34 +14,4 @@ $(function() {
   button.click(function(){
     info.slideToggle();
   });
-});
-$(function() {
-  new TWTR.Widget({
-    version: 2,
-    type: 'profile',
-    rpp: 3,
-    interval: 6000,
-    width: 274,
-    height: 'auto',
-    theme: {
-      shell: {
-        background: '#312118',
-        color: '#b16d2a'
-      },
-      tweets: {
-        background: '#312118',
-        color: '#b16d2a',
-        links: '#e68e36'
-      }
-    },
-    features: {
-      scrollbar: false,
-      loop: false,
-      live: true,
-      hashtags: true,
-      timestamp: true,
-      avatars: false,
-      behavior: 'all'
-    }
-  }).render().setUser('coreyhaines').start();
 });
