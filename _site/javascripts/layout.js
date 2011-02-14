37726c7fee590e8cafd254f344eb890e3ce5fdf9
Katie Roberts <katie@ladia-88.local>
2011-02-14 16:14:21 -0500

    hooked up accordian, finished header and footer

--- _site/javascripts/layout.js
+++ _site/javascripts/layout.js
@@ -15,3 +15,15 @@ $(function() {
     info.slideToggle();
   });
 });
+$(function(){
+
+  // Accordion
+  $("#accordion").accordion({ autoHeight: false, header: "h3" });
+  
+  //hover states on the static widgets
+  $('#dialog_link, ul#icons li').hover(
+    function() { $(this).addClass('ui-state-hover'); }, 
+    function() { $(this).removeClass('ui-state-hover'); }
+  );
+  
+});
